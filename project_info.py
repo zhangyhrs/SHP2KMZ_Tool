55f0966450525cd7c3b16f444147ca1db809c740
zhangyhrs <cumtzyh@gmail.com>
2026-09-02 07:32:35 -0700

    Add Python project metadata file

---
+++ project_info.py
@@ -0,0 +1,12 @@
+"""
+SHP2KMZ Tool
+
+Programming language: Python
+Application type: Windows desktop geospatial utility
+
+This repository distributes the compiled application and documentation.
+The complete application source code is not publicly released.
+"""
+
+PROJECT_NAME = "SHP2KMZ Tool"
+LANGUAGE = "Python"
