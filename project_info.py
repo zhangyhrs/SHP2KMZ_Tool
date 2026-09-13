ca9cc153f6d15659a62d0ff99420d1cf37622aa6
Zhang Y.H. <cumtzyh@gmail.com>
2026-09-13 06:18:20 -0700

    chore: update project metadata for v2.5.0

--- project_info.py
+++ project_info.py
@@ -2,11 +2,17 @@
 SHP2KMZ Tool
 
 Programming language: Python
-Application type: Windows desktop geospatial utility
+GUI: PyQt5
+Application type: Desktop geospatial utility
+Current version: 2.5.0
+License: GNU GPL v3.0
 
-This repository distributes the compiled application and documentation.
-The complete application source code is not publicly released.
+Copyright (c) 2026 Zhang Y.H.
 """
 
 PROJECT_NAME = "SHP2KMZ Tool"
+VERSION = "2.5.0"
 LANGUAGE = "Python"
+GUI = "PyQt5"
+LICENSE = "GPL-3.0"
+COPYRIGHT = "Copyright (c) 2026 Zhang Y.H."
